9ab05ddbc86adac4aba90ab86ad33b050048413a
AndressDanylo <andreievdanyil.se@gmail.com>
2025-01-02 19:18:41 +0200

    Added slash commands handling

--- main.py
+++ main.py
@@ -3,16 +3,20 @@ import config
 
 # bot setup
 intents = discord.Intents.default()
-intents.message_content = True
-
 client = discord.Client(intents=intents)
+tree = discord.app_commands.CommandTree(client)
 
 # functionality
 @client.event
-async def on_message(message):
-    if message. author == client.user:
-        return
-    else:
-        await message.channel.send(message.content)
+async def on_ready():
+    # Sync commands globally
+    await tree.sync()
+    print(f'We have logged in as {client.user} and synced commands.')
+
+
+@tree.command(name="quote", description="Use this command in reply to a message you want to quote")
+async def quote(interaction):
+    await interaction.response.send_message(f"Quoted by {interaction.user}")
+
 
 client.run(config.TOKEN)
