3226b3da24e3a31c1f8d898b47cc26a562bcc988
AndressDanylo <andreievdanyil.se@gmail.com>
2025-02-26 00:02:48 +0200

    Added help command

--- main.py
+++ main.py
@@ -24,9 +24,15 @@ async def quote(interaction, message: discord.Message):
 
 
 # TODO help command
-@tree.command(name="help", description="Help")
+@tree.command(name="help", description="How to use the bot")
+@discord.app_commands.allowed_contexts(guilds=True, dms=True, private_channels=True)
 async def help(interaction):
-    await interaction.response.send_message("Help")
+    embed = discord.Embed(title="How to use the bot", color=discord.Color.green())
+
+    embed.add_field(name="", value='Right click on a message and select "Quote" to convert it into a quote image.', inline=False)
+    embed.set_image(url="https://cdn.discordapp.com/attachments/1324074923429462029/1344063226085900379/help_image.png?ex=67bf8bec&is=67be3a6c&hm=c4df96072da2fd15d92fc2d109cddf834310e00770f24c9b4037f8877347b535&")
+
+    await interaction.response.send_message(embed=embed, ephemeral=True)
 
 
 client.run(config.TOKEN)
